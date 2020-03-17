553df77a57a66da85da14d3b74e441e2a2b07071
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-03-18 00:52:34 +0100

    Setup application

---
+++ hackcrisis/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HackCrisis 2020!</Text>
+      <Button onPress={() => {console.log('Test')}} title={'Teeest'} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
