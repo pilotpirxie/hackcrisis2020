4538e69a36c95dc98870ae0c2053c95a59e3fe78
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-03-22 02:31:39 +0100

    Write super simple webserver

--- webserver/app.js
+++ webserver/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
+const axios = require('axios');
+const qs = require('querystring')
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
 app.set('trust proxy', 1);
 app.disable('x-powered-by');
@@ -15,15 +21,69 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  req.setTimeout(3000);
+  req.setTimeout(8000);
   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
   next();
 });
 
-app.get('/ping', (req, res) => {
-  return res.json({
-    msg: 'pong'
-  })
+app.get('/authorities/:id', (req, res) => {
+  const data = db.get(`authorities`).find({id: req.params.id}).value();
+  if (data) {
+    return res.json(data);
+  } else {
+    return res.sendStatus(404);
+  }
+});
+
+app.get('/authorities', (req, res) => {
+  const data = db.get(`authorities`).value();
+  if (data && data.length > 0) {
+    return res.json(data);
+  } else {
+    return res.sendStatus(404);
+  }
+});
+
+app.post('/authorities/:id/email', async (req, res) => {
+  try {
+    const {requestType, message, userId} = req.body;
+
+    const authorityData = db.get(`authorities`).find({id: req.params.id}).value();
+    const userData = db.get(`users`).find({id: userId}).value();
+
+    if (!authorityData || !userData) {
+      return res.sendStatus(404);
+    }
+    const requestData = {
+      access_token: '6971c836-6bd8-11ea-bc55-0242ac130003',
+      to: authorityData.email,
+      subject: `${requestType} - ${userData.name} ${userData.surname}`,
+      message: `
+        Imię i nazwisko: ${userData.name} ${userData.surname}
+        Wiek: ${userData.age} 
+        Identyfikator: ${userId}
+        Email: ${userData.email} 
+        Lokalizacja deklarowana: ${userData.location}
+        
+        Wiadomość: 
+        ${message}
+      `
+    };
+    console.log(requestData);
+
+    await axios.post(
+      'https://hackcrisis.gamenado.com/mail.php',
+      qs.stringify(requestData),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+    res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(400);
+  }
 });
 
 try {
